fa6188438fb40e611b4b78a84206caa52527ec36
danac33 <danac.opasni@gmail.com>
2024-06-03 23:01:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+function thx() {
+    alert("hvala na poruci");
+    location.reload();
+}
+
+function contactC() {
+    const myTimeout = setTimeout(g, 1000);
+}
+
+function g() {
+    alert("ako me zelite kontaktirati na drugi nacin:+387 62 633 883 ili danac_opasni@gmail.com")
+    clearTimeout(myTimeout);
+}
+
 function scrollToSection(className) {
     var element = document.querySelector(className);
     if (element) {
         element.scrollIntoView({ behavior: 'smooth' });
     }
 }
-alert("this took soo long")
+
