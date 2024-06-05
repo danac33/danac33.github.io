115c1c3215ae15be48a952a571053066438ac312
danac33 <danac.opasni@gmail.com>
2024-06-05 12:49:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
-function thx() {
-    alert("hvala na poruci");
-    location.reload();
+let inputt2 = document.getElementById("sassy2");
+
+function thx(j) {
+      if (j.value.length == 0)
+       { 
+          return false; 
+       } else  {
+        alert("hvala na poruci")
+        return true;
+       }
+     
+
+}
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
 
 function contactC() {
-    const myTimeout = setTimeout(g, 1000);
+  const myTimeout = setTimeout(g, 1000);
 }
 
 function g() {
-    alert("ako me zelite kontaktirati na drugi nacin:+387 62 633 883 ili danac_opasni@gmail.com")
-    clearTimeout(myTimeout);
+  alert(
+    "ako me zelite kontaktirati na drugi nacin:+387 62 633 883 ili danac_opasni@gmail.com"
+  );
+  clearTimeout(myTimeout);
 }
 
 function scrollToSection(className) {
-    var element = document.querySelector(className);
-    if (element) {
-        element.scrollIntoView({ behavior: 'smooth' });
-    }
+  var element = document.querySelector(className);
+  if (element) {
+    element.scrollIntoView({ behavior: "smooth" });
+  }
 }
-
